d008e1381ffecd831d7ee86e6a580f5aded93c33
MihailKuz77 <oper2c@gmail.com>
2026-06-24 09:15:14 +0300

    Create accept_license.sh

---
+++ .buildozer/accept_license.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Auto-accept Android SDK licenses
+mkdir -p $HOME/.buildozer/android/platform/android-sdk/licenses
+echo -e "\n24333f8a63b6825ea9c5514f83c2829b004d1fee" > $HOME/.buildozer/android/platform/android-sdk/licenses/android-sdk-license
